985f8eb918d6e671bea14ed4f24bc86135545f71
cityshin <ssk7750@naver.com>
2024-04-28 19:32:28 +0900

    feature. 백준

--- seungkyun/src/BaekJoon/BaekJoon_16926.java
+++ seungkyun/src/BaekJoon/BaekJoon_16926.java
@@ -7,11 +7,11 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 
-
     static int[][] array;
     static int[][] newArray;
     static int N,M,R;
 
+    static int size;
     public static void main(String[] args) throws IOException {
 
 
@@ -23,7 +23,7 @@ public class Main {
 
         array = new int[N][M];
         newArray = new int[N][M];
-//        newArray = array;
+
         for(int i=0; i<N;i++){
             StringTokenizer st2 = new StringTokenizer(br.readLine());
             for(int j=0; j<M;j++){
@@ -35,7 +35,7 @@ public class Main {
         }
 
 
-        evenNevenM();
+        find();
 
 
         for(int i=0; i<N;i++){
@@ -47,13 +47,11 @@ public class Main {
         }
 
 
-
-
         bw.flush();
     }
 
-    //둘다 짝수
-    static void evenNevenM(){
+
+    static void find() {
 
         //끝 모서리
         int[] upStart = {0,0};
@@ -62,19 +60,19 @@ public class Main {
         int[] rightStart= {0,M-1};
 
         for(int i=0; i<R;i++){
+
             while(upStart[0]<downStart[0]&&rightStart[1]>leftStart[1]){
 
                 //위
                 for(int up=upStart[1]; up<rightStart[1];up++){
                     array[upStart[0]][up] = newArray[upStart[0]][up+1];
-
                 }
                 //왼쪽
-                for(int left=leftStart[0];left>=upStart[0];left--){
+                for(int left=leftStart[0];left>upStart[0];left--){
                     array[left][leftStart[1]] = newArray[left-1][leftStart[1]];
                 }
                 //아래
-                for(int down=downStart[1];down>=leftStart[1];down--){
+                for(int down=downStart[1];down>leftStart[1];down--){
                     array[downStart[0]][down] = newArray[downStart[0]][down-1];
                 }
                 //오른쪽
@@ -90,6 +88,7 @@ public class Main {
                 leftStart[0]--;
                 leftStart[1]++;
 
+                size++;
             }
             for(int n=0; n<N;n++){
                 for(int m=0; m<M;m++){
@@ -98,21 +97,25 @@ public class Main {
                 }
             }
 
+            for(int s=0; s<size;s++){
+                upStart[0]--;
+                upStart[1]--;
+                downStart[0]++;
+                downStart[1]++;
+                rightStart[0]--;
+                rightStart[1]++;
+                leftStart[0]++;
+                leftStart[1]--;
+
+            }
+            size=0;
+
+
 
 
         }
 
     }
-    //N홀수
-    static void oddNevenM(){
-
-    }
-    //M홀수
-    static void evenNoddM(){
-
-    }
-
-
 
 
 
